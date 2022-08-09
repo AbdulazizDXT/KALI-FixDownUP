d0016147eef9968986492c7a72f106ca57fa5ae4
AbdulazizDXT <105378232+AbdulazizDXT@users.noreply.github.com>
2022-08-09 14:54:30 +0400

    Update FixedScript.sh

--- FixedScript.sh
+++ FixedScript.sh
@@ -1,6 +1,5 @@
-#!bin/bash
-echo "deb http://kali.download/kali kali-rolling main contrib non-free" | sudo tee /etc/apt/sources.list
-echo "deb http://kali.download/kali kali-last-snapshot main contrib non-free" | sudo tee /etc/apt/sources.list
-echo "deb http://kali.download/kali kali-experimental main contrib non-free" | sudo tee /etc/apt/sources.list
-echo "deb http://kali.download/kali kali-bleeding-edge main contrib non-free" | sudo tee /etc/apt/sources.list
+echo "deb http://kali.download/kali kali-rolling main contrib non-free
+deb http://kali.download/kali kali-last-snapshot main contrib non-free
+deb http://kali.download/kali kali-experimental main contrib non-free
+deb http://kali.download/kali kali-bleeding-edge main contrib non-free" | sudo t>
 sudo apt-get update -y
