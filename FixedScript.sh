0f1bf601ec9e4b1944f594344148b808432a2974
AbdulazizDXT <105378232+AbdulazizDXT@users.noreply.github.com>
2022-08-09 15:05:02 +0400

    Update FixedScript.sh

--- FixedScript.sh
+++ FixedScript.sh
@@ -1,6 +1,6 @@
 echo "deb http://kali.download/kali kali-rolling main contrib non-free
 deb http://kali.download/kali kali-last-snapshot main contrib non-free
 deb http://kali.download/kali kali-experimental main contrib non-free
-deb http://kali.download/kali kali-bleeding-edge main contrib non-free" | sudo t>
+deb http://kali.download/kali kali-bleeding-edge main contrib non-free" | sudo tee /etc/apt/sources.list
 sudo apt-get update -y
 type THX.txt
